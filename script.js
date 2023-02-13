c47c8f6b9ba241952b0fc4649a2ff012bb3464c6
CatSink <raindropwebweaver@live.com>
2023-02-12 19:52:25 -0500

    fixed bugs in JS

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ var password=document.getElementById("password");
    var randomNumber = Math.floor(Math.random() * chars.length);
    password += chars.substring(randomNumber, randomNumber +1);
   }
-        document.getElementById("password").value = password;
+        document.getElementById("password").value = "";
  }
 
 function generatePassword() { 
@@ -38,7 +38,6 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("Generated Password");
 
-  passwordText.value ="password";
 
 }
 
